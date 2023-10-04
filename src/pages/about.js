694b8b1a283a22b8f44ed1228f8b25fd8473e35b
Falla <farninn@gmail.com>
2023-10-04 07:40:56 +0200

    Finished Gatsby Tutorial Part 3

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+import { StaticImage } from "gatsby-plugin-image";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <Layout pageTitle="About Falla">
+        <StaticImage alt="Potatos" src="../images/Patates.jpg"></StaticImage>
+        <p>Decent at everything!</p>
+      </Layout>
+    </main>
+  );
+};
+
+export const Head = () => <title>Big Scrub</title>;
+
+export default AboutPage;
